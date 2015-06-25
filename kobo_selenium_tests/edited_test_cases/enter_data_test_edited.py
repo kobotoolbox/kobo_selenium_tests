08001cf3c58fd25d9c6b0b5cafefffdc9613e69b
Esmail <esmail.fadae@kobotoolbox.org>
2015-06-25 13:25:06 -0400

    Don't sleep when watching for the Enketo submission queue to increment.

--- kobo_selenium_tests/edited_test_cases/enter_data_test_edited.py
+++ kobo_selenium_tests/edited_test_cases/enter_data_test_edited.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
+import unittest
+import time
+import re
+
 from selenium import webdriver
+from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+
 
 class EnterDataTestEdited(unittest.TestCase):
+
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
@@ -22,66 +27,99 @@ class EnterDataTestEdited(unittest.TestCase):
         driver.find_element_by_link_text("Selenium test form title.").click()
         for i in range(60):
             try:
-                if self.is_element_present(By.CSS_SELECTOR, ".dashboard__button-enter-data"): break
-            except: pass
+                if self.is_element_present(By.CSS_SELECTOR, ".dashboard__button-enter-data"):
+                    break
+            except:
+                pass
             time.sleep(1)
-        else: self.fail("time out")
-        data_entry_url = driver.find_element_by_css_selector(".dashboard__button-enter-data").get_attribute("href")
-        # WARNING: The 'open' command doesn't export correctly to Python, always behaving as if a relative address has been provided, so a manual edit is necessary.
+        else:
+            self.fail("time out")
+        data_entry_url = driver.find_element_by_css_selector(
+            ".dashboard__button-enter-data").get_attribute("href")
+        # WARNING: The 'open' command doesn't export correctly to Python, always
+        # behaving as if a relative address has been provided, so a manual edit is
+        # necessary.
         driver.get(data_entry_url)
         for i in range(60):
             try:
-                if self.is_element_present(By.ID, "form-title"): break
-            except: pass
+                if self.is_element_present(By.ID, "form-title"):
+                    break
+            except:
+                pass
             time.sleep(1)
-        else: self.fail("time out")
-        self.assertEqual("Selenium test form title.", driver.find_element_by_css_selector("#form-title").text)
-        self.assertEqual("Selenium test question label.", driver.find_element_by_css_selector(".question-label").text)
-        self.assertEqual("Selenium test question choice 1.", driver.find_element_by_css_selector(".question label:nth-child(1) .option-label").text)
-        self.assertEqual("Selenium test question choice 2.", driver.find_element_by_css_selector(".question label:nth-child(2) .option-label").text)
+        else:
+            self.fail("time out")
+        self.assertEqual(
+            "Selenium test form title.", driver.find_element_by_css_selector("#form-title").text)
+        self.assertEqual(
+            "Selenium test question label.", driver.find_element_by_css_selector(".question-label").text)
+        self.assertEqual("Selenium test question choice 1.", driver.find_element_by_css_selector(
+            ".question label:nth-child(1) .option-label").text)
+        self.assertEqual("Selenium test question choice 2.", driver.find_element_by_css_selector(
+            ".question label:nth-child(2) .option-label").text)
         driver.find_element_by_css_selector(".question label:nth-child(1)").click()
         for i in range(60):
             try:
-                if "0" == driver.find_element_by_css_selector(".queue-length").text: break
-            except: pass
+                if "0" == driver.find_element_by_css_selector(".queue-length").text:
+                    break
+            except:
+                pass
             time.sleep(1)
-        else: self.fail("time out")
+        else:
+            self.fail("time out")
         self.assertTrue(self.is_element_present(By.ID, "submit-form"))
         driver.find_element_by_id("submit-form").click()
         # Ensure that the data was submitted and no alerts were generated.
         for i in range(60):
             try:
-                if "1" == driver.find_element_by_css_selector(".queue-length").text: break
-            except: pass
-            time.sleep(1)
-        else: self.fail("time out")
-        for i in range(60): # FIXME: We consistently timeout waiting for Enketo to clear its queue; find the cause.
+                if "1" == driver.find_element_by_css_selector(".queue-length").text:
+                    break
+            except:
+                pass
+            # This is a momentary occurrence, so try hard to catch it.
+            # time.sleep(1)
+        else:
+            self.fail("time out")
+        # FIXME: We consistently timeout waiting for Enketo to clear its queue; find the cause.
+        for i in range(60):
             try:
-                if "0" == driver.find_element_by_css_selector(".queue-length").text: break
-            except: pass
+                if "0" == driver.find_element_by_css_selector(".queue-length").text:
+                    break
+            except:
+                pass
             time.sleep(1)
-        else: self.fail("time out")
-        self.assertEqual("", driver.find_element_by_css_selector("#dialog-alert .modal-header").text)
+        else:
+            self.fail("time out")
+        self.assertEqual(
+            "", driver.find_element_by_css_selector("#dialog-alert .modal-header").text)
         driver.get(self.base_url + "selenium_test/forms/Selenium_test_form_title")
         # Close out the "unsaved changes" warning.
         if self.is_alert_present():
             driver.switch_to_alert().accept()
         for i in range(60):
             try:
-                if self.is_element_present(By.CSS_SELECTOR, ".dashboard__submissions .dashboard__group-label"): break
-            except: pass
+                if self.is_element_present(By.CSS_SELECTOR, ".dashboard__submissions .dashboard__group-label"):
+                    break
+            except:
+                pass
             time.sleep(1)
-        else: self.fail("time out")
-        self.assertEqual("Submissions (1)", driver.find_element_by_css_selector(".dashboard__submissions .dashboard__group-label").text)
+        else:
+            self.fail("time out")
+        self.assertEqual("Submissions (1)", driver.find_element_by_css_selector(
+            ".dashboard__submissions .dashboard__group-label").text)
 
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException, e:
+            return False
         return True
 
     def is_alert_present(self):
-        try: self.driver.switch_to_alert()
-        except NoAlertPresentException, e: return False
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException, e:
+            return False
         return True
 
     def close_alert_and_get_its_text(self):
@@ -93,7 +131,8 @@ class EnterDataTestEdited(unittest.TestCase):
             else:
                 alert.dismiss()
             return alert_text
-        finally: self.accept_next_alert = True
+        finally:
+            self.accept_next_alert = True
 
     def tearDown(self):
         self.driver.quit()
