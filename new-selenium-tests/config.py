3c37314ef700461e2f69f8260e43752745bdfdd6
Yahya Zini <yzini@bitbucket.com>
2017-03-21 19:08:51 -0400

    Run tests with flags

---
+++ new-selenium-tests/config.py
@@ -0,0 +1,17 @@
+environments = {
+    'test': {
+        'base_url' : 'http://kf.kobotoolbox.org/',
+        'username': 'selenium_test',
+        'password': 'selenium_test'
+    },
+    'production': {
+        'base_url' : 'http://kf.kobotoolbox.org/',
+        'username': 'selenium_test',
+        'password': 'selenium_test'
+    },
+    'local': {
+        'base_url' : 'http://172.17.0.1:8000/',
+        'username': 'admin',
+        'password': 'admin'
+    }
+}
