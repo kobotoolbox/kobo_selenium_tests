eb46d585ec6c4d83ad8334f07a8076a8b618698c
Yahya Zini <yahyazini@gmail.com>
2017-06-16 14:06:27 -0400

    Updated tests/new_form_from_scratch_test.py

--- new-selenium-tests/tests/new_form_from_scratch_test.py
+++ new-selenium-tests/tests/new_form_from_scratch_test.py
@@ -13,37 +13,39 @@ class NewFormFromScratchTest(empty_test.EmptyTest):
             driver = self.driver
             self.mouse = webdriver.ActionChains(self.driver)
             #go to enwe form page and click on the sidbar menu
-            driver.get(self.base_url + "#/forms/new")
-            self.assertTrue(self.is_element_present_with_wait(By.ID, "sidebar-menu", 30))
-            el = driver.find_element_by_id("sidebar-menu")
-            self.mouse.move_to_element(el).click()
+            driver.get(self.base_url + "#/forms")
+            driver.maximize_window()
 
-            #fill form name
-            new_form_button = driver.find_element_by_css_selector(".collection-nav__link--new")
-            self.mouse.move_to_element(new_form_button).click()
-            time.sleep(1)
-            self.assertTrue(self.is_element_present_with_wait(By.ID, "name"), 30)
+            self.clickSideBarNewBtn()
+
+            new_project_btn_selector = ".form-sidebar__wrapper .popover-menu__content--visible > .popover-menu__link"
+            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, new_project_btn_selector))
+            new_project_btn = driver.find_element_by_css_selector(new_project_btn_selector)
+            new_project_btn.click()
+
+            self.assertTrue(self.is_element_present_with_wait(By.ID, "name"))
             driver.find_element_by_css_selector("#name").send_keys("My Awesome KoboToolbox Form")
 
             #fill form description
-            self.assertTrue(self.is_element_present_with_wait(By.ID, "description"), 30)
-            driver.find_element_by_css_selector("#description").send_keys("My form's description")
+            description_selector= ".form-modal__item:nth-child(2) textarea"
+            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, description_selector))
+            driver.find_element_by_css_selector(description_selector).send_keys("My form's description")
 
             #select form sector
             sector_input = ".form-modal__item--sector .Select-input input"
-            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, sector_input), 30)
+            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, sector_input))
             sector_el = driver.find_element_by_css_selector(sector_input)
             sector_el.send_keys("Public Administration")
             sector_el.send_keys(Keys.ENTER)
 
             #fill country input
             country_input = ".form-modal__item--country .Select-input input"
-            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, country_input), 30)
+            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, country_input))
             country_el = driver.find_element_by_css_selector(country_input)
             country_el.send_keys("United States")
             country_el.send_keys(Keys.ENTER)
 
-            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, ".form-modal__item--actions button"), 30)
+            self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, ".form-modal__item--actions button"))
             submit_button = driver.find_element(By.CSS_SELECTOR, ".form-modal__item--actions button")
             submit_button.send_keys(Keys.ENTER)
 
