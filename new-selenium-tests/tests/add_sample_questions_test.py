e266c134e79a1ed4724fb500d853072a9e754e0b
Yahya Zini <yahyazini@gmail.com>
2017-06-16 14:05:14 -0400

    updating tests/add_sample_questions_test.py

--- new-selenium-tests/tests/add_sample_questions_test.py
+++ new-selenium-tests/tests/add_sample_questions_test.py
@@ -22,7 +22,7 @@ class AddSampleQuestionsTest(empty_test.EmptyTest):
             save_btn_selector = ".formBuilder-header__button--save"
             self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, save_btn_selector, 10))
             driver.find_element_by_css_selector(save_btn_selector).send_keys(Keys.ENTER)
-            time.sleep(3)
+            self.assertTrue(self.is_element_present_with_wait(By.XPATH, "//div[text()='successfully updated']"))
 
             #close the modal
             close_btn_selector = ".formBuilder-header__close"
