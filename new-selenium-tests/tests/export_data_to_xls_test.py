be6eec2a8501a362ccf97da2f9d789c1ca8b5857
Marjorie Roswell <mroswell+github@gmail.com>
2018-02-25 13:58:42 -0500

    switch to Data tab to access "/data/downloads"

--- new-selenium-tests/tests/export_data_to_xls_test.py
+++ new-selenium-tests/tests/export_data_to_xls_test.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium                                       import webdriver
+from selenium.webdriver.support                     import expected_conditions as EC
+from selenium.webdriver.common.by                   import By
 import unittest
 import empty_test
 import time
@@ -19,8 +20,20 @@ class ExportDataToXls(empty_test.EmptyTest):
             self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, form_link))
             form_link_el = driver.find_elements_by_css_selector(form_link)
             form_link_el[0].click()
+            print driver.current_url
 
-            driver.get(driver.current_url.replace("/landing", "/data/downloads"))
+            # switch to Data tab
+            data_tab_el = driver.wait.until(EC.presence_of_element_located(
+                (By.XPATH, "//a[contains(@class, 'form-view__tab') and text()='Data']")
+            ))
+            data_tab_el.click()
+
+            download_el = driver.wait.until(EC.presence_of_element_located(
+                (By.XPATH, "//a[contains(@class, 'form-view__tab undefined') and text()='Downloads']")
+            ))
+            download_el.click()
+
+            # driver.get(driver.current_url.replace("/landing", "/data/downloads"))
 
             #make sure you're on the data/downloads page
             self.assertTrue(self.is_element_present_with_wait(By.XPATH, "//label[text()='Select export type']"))
@@ -37,8 +50,9 @@ class ExportDataToXls(empty_test.EmptyTest):
                 # When using a remote `webdriver`, we don't have direct access to downloaded files.
                 #   Instead, inspect the remote Selenium server's filesystem with its browser.
                 driver.get('file:///tmp/')
-                # TODO: Fix this kludge with a pure Xpath solution?
-                local_file_link = driver.find_element_by_partial_link_text('My_Awesome_Kobo_Form')
+            #     # TODO: Fix this kludge with a pure Xpath solution?
+                # local_file_link = driver.find_element_by_partial_link_text('My_Awesome_Kobo_Form')
+                local_file_link = driver.find_element_by_partial_link_text('.xls')
                 file_size =  local_file_link.find_element_by_xpath('../../td[2]').text
                 self.status('Downloaded {} file `{}`.'.format(file_size, local_file_link.text))
             elif not self.does_file_exist_with_wildcard("/tmp/My_Awesome_Kobo_Form.xls"):
