e7fae39d36cba1f8c30426171e0b6af074852aa5
Marjorie Roswell <mroswell+github@gmail.com>
2018-03-04 22:00:11 -0500

    fix default time comment and PEP8 compliance

--- new-selenium-tests/tests/add_sample_questions_test.py
+++ new-selenium-tests/tests/add_sample_questions_test.py
@@ -10,22 +10,24 @@ import empty_test
 class AddSampleQuestionsTest(empty_test.EmptyTest):
 
     def add_questions_test(self):
-        try :
+        try:
             driver = self.driver
             self.mouse = webdriver.ActionChains(self.driver)
-            #create a new empty question and wait for 3 seconds by default or add a 3rd parameter with value of seconds to wait | this function is a helper function from the empty_test class
+            # create a new empty question and wait for one second by default
+            # or add a third parameter with value of seconds to wait
+            # this function is a helper function from the empty_test class
             self.add_new_question("Name", "text")
             self.add_new_question("LastName", "text")
             self.add_new_question("Avatar", "image")
             self.add_new_question("Gender", "select_one")
 
-            #click the save button
+            # click the save button
             save_btn_selector = ".formBuilder-header__button--save"
             self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, save_btn_selector, 10))
             driver.find_element_by_css_selector(save_btn_selector).send_keys(Keys.ENTER)
             self.assertTrue(self.is_element_present_with_wait(By.XPATH, "//div[text()='successfully updated']"))
 
-            #close the modal
+            # close the modal
             close_btn_selector = ".formBuilder-header__close"
             self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, close_btn_selector, 10))
             driver.find_element_by_css_selector(close_btn_selector).send_keys(Keys.ENTER)
