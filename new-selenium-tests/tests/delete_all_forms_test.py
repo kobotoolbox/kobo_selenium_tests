78731500aba436e68144db86ef5edf8dd34a965d
Marjorie Roswell <mroswell+github@gmail.com>
2018-03-04 21:59:07 -0500

    PEP8 compliance

--- new-selenium-tests/tests/delete_all_forms_test.py
+++ new-selenium-tests/tests/delete_all_forms_test.py
@@ -6,6 +6,7 @@ import empty_test
 import time
 from selenium.common.exceptions import NoAlertPresentException
 
+
 class DeleteAllFormsTest(empty_test.EmptyTest):
 
     def delete_all_forms(self):
@@ -15,11 +16,11 @@ class DeleteAllFormsTest(empty_test.EmptyTest):
             self.mouse = webdriver.ActionChains(self.driver)
             driver.get(self.base_url + "#/forms")
 
-            #Hover over the assets action buttons
+            # Hover over the assets action buttons
             form_link = ".asset-row__buttons"
             # Quick Check
             time.sleep(2)
-            if(self.is_element_present(By.CSS_SELECTOR, form_link)):
+            if self.is_element_present(By.CSS_SELECTOR, form_link):
                 form_link_list = driver.find_elements_by_css_selector(form_link)
                 for form in form_link_list:
                     # print type(self)
@@ -31,6 +32,7 @@ class DeleteAllFormsTest(empty_test.EmptyTest):
 
         except Exception as e:
             self.handle_test_exceptions(e)
+
 
 if __name__ == "__main__":
     unittest.main()
