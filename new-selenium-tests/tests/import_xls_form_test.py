a061d490b63ddce144ef3ab68180d666a9fff51d
Marjorie Roswell <mroswell+github@gmail.com>
2018-05-21 04:46:56 -0400

    account for different naming convention of tmp file

--- new-selenium-tests/tests/import_xls_form_test.py
+++ new-selenium-tests/tests/import_xls_form_test.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 import os
 import unittest
@@ -12,11 +14,17 @@ class ImportXlsFormTest(empty_test.EmptyTest):
     def test_import_xls_form(self):
         try:
             driver = self.driver
-            driver.implicitly_wait(0)
+            driver.wait = WebDriverWait(driver, 5)
             self.mouse = webdriver.ActionChains(self.driver)
             driver.get(self.base_url + "#/forms")
 
-            self.clickSideBarNewBtn()
+            self.startNewProject("My Awesome KoboToolbox Form")
+
+            # Complete "Create New Project (step 2 of 2)"
+            upload_xls_button_el = driver.wait.until(EC.presence_of_element_located(
+                (By.XPATH, "//button[text() = 'Upload an XLSForm']")
+            ))
+            upload_xls_button_el.click()
 
             # click on the upload button
             self.assertTrue(self.is_element_present_with_wait(By.CSS_SELECTOR, ".dropzone input[type='file']"))
@@ -24,7 +32,7 @@ class ImportXlsFormTest(empty_test.EmptyTest):
             driver.find_element_by_css_selector(".dropzone input[type='file']").send_keys(xls_file)
 
             # Make sure the form was uploaded
-            self.assertTrue(self.is_element_present_with_wait(By.XPATH, "//div[text()='XLS Upload completed']"))
+            self.assertTrue(self.is_element_present_with_wait(By.XPATH, "//div[text()='XLS Import completed']"))
 
             # This is an empty_test method!
             self.generic_preview_form()
